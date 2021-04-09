5efe352d700dd2ae93f09d49c59335024bca94cf
paulotesta <paulo.testa@icloud.com>
2021-04-09 01:18:22 +0100

    final build of the program and README update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # hang_man
-Hangman game
+
+## _Sample Python game_
+
+Hang Man is the famous game coded using Python programing.
+
+## Instructions
+
+To start the program you'll need python 3 installed on your system and type:
+
+```sh
+python3 hang_man.py
+```
+
+The user must select one character at a time and try to guess the correct word, using the blank spaces as guidance.
+Each round either the user selected a letter that is present in the word or not. If the letter is present, it will fill the blanks. If the letter is incorrect, the figure of the hang man will start to be drawn and the user will have another chance to get it right.
+The games end with the user guessing the word correctly or hanging the man.
--- hang_man.py
+++ hang_man.py
@@ -2,16 +2,21 @@ import random
 
 WORD_OPTIONS = ('test', 'gata', 'computer', 'program')
 NUM_OF_TRIES = 10
+ALLOWED_CHAR = 'abcdefghijklmnopqrstuvwxyz'
 
 
 def main():
+    all_guesses = ''
     initial_instructions()
     word = present_word(WORD_OPTIONS)
-    tries = 0
-    while tries < NUM_OF_TRIES:
+    result = [0, 0]
+    while result[0] < NUM_OF_TRIES:
         letter = get_letter()
-        result = compare_letter(letter, word[0], word[1])
-        draw_hangman(result)
+        all_guesses = all_guesses + letter
+        result = compare_letter(letter, all_guesses, word, result[0])
+        if result[1] == word:
+            winner(word)
+            break
 
 
 def initial_instructions():
@@ -27,32 +32,106 @@ def present_word(WORD_OPTIONS):
     char_count = len(word)
     masked_word = "_ " * char_count
     print(masked_word)
-    return word, masked_word
+    return word
 
 
 def get_letter():
-    print("Plese type your guess:")
+    print("Plese type your guess, one letter at a time:")
     guess = input()
-    return guess
+
+    while (len(guess) != 1 or guess.lower() not in ALLOWED_CHAR):
+        print("Please only use Alphabetical characters, try again")
+        print("Plese type your guess:")
+        guess = input()
+
+    return guess.lower()
 
 
-def compare_letter(letter, word, masked_word):
-    i = 0
-    for element in word:
-        if letter == element:
-            masked_word = draw_word(masked_word, letter, i)
-        i = i + 1
-    print(masked_word)
+def compare_letter(letter, all_guesses, word, tries):
+    output = ''
+
+    if letter not in word:
+        tries = incorrect_guess(tries)
+
+    for letter in word:
+        if letter in all_guesses:
+            output = output + letter
+        else:
+            output = output + '_ '
+
+    print(output)
+
+    return tries, output
 
 
-def draw_word(masked_word, letter, i):
-    masked_word = masked_word[:i*2] + letter + masked_word[(i * 2) + 1:]
+def draw_hangman(image):
+    if image == 1:
+        print("  --------  ")
+        return
+    if image == 2:
+        print("  --------  ")
+        print("     O      ")
+        return
+    if image == 3:
+        print("  --------  ")
+        print("     O      ")
+        print("     |      ")
+        return
+    if image == 4:
+        print("  --------  ")
+        print("     O      ")
+        print("     |      ")
+        print("    /       ")
+        return
+    if image == 5:
+        print("  --------  ")
+        print("     O      ")
+        print("     |      ")
+        print("    / \     ")
+        return
+    if image == 6:
+        print("  --------  ")
+        print("   \ O      ")
+        print("     |      ")
+        print("    / \     ")
+        return
+    if image == 7:
+        print("  --------  ")
+        print("   \ O /    ")
+        print("     |      ")
+        print("    / \     ")
+        return
+    if image == 8:
+        print("  --------  ")
+        print("   \ O /|   ")
+        print("     |      ")
+        print("    / \     ")
+        return
+    if image == 9:
+        print("  --------  ")
+        print("   \ O_|/   ")
+        print("     |      ")
+        print("    / \     ")
+        return
+    if image == 10:
+        print("  --------  ")
+        print("     O_|    ")
+        print("    /|\     ")
+        print("    / \     ")
+        print("You Loose!")
+        return
 
-    return masked_word
+
+def incorrect_guess(tries):
+    tries = tries + 1
+    print("Guesses remaning: ", NUM_OF_TRIES - tries)
+    draw_hangman(tries)
+    return tries
 
 
-def draw_hangman(result):
-    pass
+def winner(word):
+    print("Congratulations! You correctly guessed: ", word)
+    return
 
 
 main()
